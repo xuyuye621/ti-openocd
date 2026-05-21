c52e3bfc5478556549b3befa2f10b82c65b9ca44
Animesh Dhara <a-dhara@ti.com>
2026-05-21 13:48:30 +0530

    LPRFTOOLS-1758: Removed old logs that had wrong information about CC27xx banks.

--- src/flash/nor/cc_lpf3_base.c
+++ src/flash/nor/cc_lpf3_base.c
@@ -291,7 +291,6 @@ int cc_lpf3_base_probe(struct flash_bank *bank)
         bank->num_sectors = 0x1;
         break;
     case LPF3_FLASH_BASE_MAIN:
-        /* For CC23XX, main_flash_num_banks is 1, for CC27XX it's 2 */
         bank->size = (cc_lpf3_info->main_flash_size_kb * 1024) / cc_lpf3_info->main_flash_num_banks;
         bank->num_sectors = (bank->size) / (LPF3_MAIN_FLASH_SECTOR_SIZE);
         break;
